1ffa2c15108491e5e97de49d235f02c1ec855a94
robertIsaac <robert.isaac@live.com>
2022-04-30 02:32:40 +0200

    remove AppModule and use standalone AppComponent

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+
+const routes: Routes = [];
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    ...importProvidersFrom(RouterModule.forRoot(routes)),
+  ],
+})
   .catch(err => console.error(err));
